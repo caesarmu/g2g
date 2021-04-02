99f70a3adb32e9d46b41fe55729b6c071ad566fb
Mu <caesarmu@users.noreply.github.com>
2021-04-02 19:15:32 +0800

    Update g2g4w.bat

--- g2g4w.bat
+++ g2g4w.bat
@@ -18,6 +18,7 @@ IF "%ingeoext%0%outgeo%" == ".vasp0" (busybox64.exe sh -c "cd %cd:\=/%; v2pw %in
 IF "%ingeoext%0%outgeoext%" == ".out0.xsf" (busybox64.exe sh -c "cd %cd:\=/%; pw2xsf %ingeo% %outgeo%"  && goto END )
 IF "%ingeoext%0%outgeoext%" == ".out0.cif" (busybox64.exe sh -c "cd %cd:\=/%; pw2xsf %ingeo% g2g4wtmp.xsf && obabel.exe g2g4wtmp.xsf -O%outgeo%"  && goto END )
 IF "%ingeoext%0%outgeoext%" == ".out0.pw" (busybox64.exe sh -c "cd %cd:\=/%; pw2xsf %ingeo% g2g4wtmp.xsf && obabel.exe g2g4wtmp.xsf -ovasp -Og2g4wtmp.vasp && v2pw g2g4wtmp.vasp %outgeo%"  && goto END )
+IF "%ingeoext%0%outgeoext%" == ".out0.vasp" (busybox64.exe sh -c "cd %cd:\=/%; pw2xsf %ingeo% g2g4wtmp.xsf && obabel.exe g2g4wtmp.xsf -ovasp %outgeo%"  && goto END )
 IF "%ingeoext%0%outgeo%" == ".out0" (busybox64.exe sh -c "cd %cd:\=/%; pw2xsf %ingeo% %ingeobase%.xsf" && goto END )
 IF "%ingeoext%0%outgeoext%" == ".pw0.xsf" (busybox64.exe sh -c "cd %cd:\=/%; pw2xsf %ingeo% %outgeo%"  && goto END )
 IF "%ingeoext%0%outgeoext%" == ".pw0.cif" (busybox64.exe sh -c "cd %cd:\=/%; pw2xsf %ingeo% g2g4wtmp.xsf && obabel.exe g2g4wtmp.xsf -O%outgeo%"  && goto END )
